94e58f7587c28c23b6d2c2859d8384ea646b181b
Hutu Tudor <hutu.tudor@yahoo.com>
2023-09-10 13:50:11 +0300

    feat(cpu): add indirect and relative address modes

--- src/cpu/address_modes.c
+++ src/cpu/address_modes.c
@@ -36,7 +36,13 @@ address_mode_t cpu_address_mode_zp_y(cpu_t* cpu) {
   return address_mode;
 }
 
-address_mode_t cpu_address_mode_relative(cpu_t* cpu) { ASSERT_UNREACHABLE; }
+address_mode_t cpu_address_mode_relative(cpu_t* cpu) {
+  address_mode_t address_mode = {0};
+  s8 relative = cpu_fetch8(cpu);
+  address_mode.address = relative + cpu->pc;
+  address_mode.value = bus_read8(cpu->bus, address_mode.address);
+  return address_mode;
+}
 
 address_mode_t cpu_address_mode_absolute(cpu_t* cpu) {
   address_mode_t address_mode = {0};
@@ -59,8 +65,26 @@ address_mode_t cpu_address_mode_absolute_y(cpu_t* cpu) {
   return address_mode;
 }
 
-address_mode_t cpu_address_mode_indirect(cpu_t* cpu) { ASSERT_UNREACHABLE; }
+address_mode_t cpu_address_mode_indirect(cpu_t* cpu) {
+  address_mode_t address_mode = {0};
+  u16 address = cpu_fetch16(cpu);
+  address_mode.address = bus_read16(cpu->bus, address);
+  address_mode.value = bus_read8(cpu->bus, address_mode.address);
+  return address_mode;
+}
 
-address_mode_t cpu_address_mode_indirect_x(cpu_t* cpu) { ASSERT_UNREACHABLE; }
+address_mode_t cpu_address_mode_indirect_x(cpu_t* cpu) {
+  address_mode_t address_mode = {0};
+  u8 address = cpu_fetch8(cpu) + cpu->x;
+  address_mode.address = bus_read16(cpu->bus, address);
+  address_mode.value = bus_read8(cpu->bus, address_mode.address);
+  return address_mode;
+}
 
-address_mode_t cpu_address_mode_indirect_y(cpu_t* cpu) { ASSERT_UNREACHABLE; }
+address_mode_t cpu_address_mode_indirect_y(cpu_t* cpu) {
+  address_mode_t address_mode = {0};
+  u8 address = cpu_fetch8(cpu);
+  address_mode.address = bus_read16(cpu->bus, address) + cpu->y;
+  address_mode.value = bus_read8(cpu->bus, address_mode.address);
+  return address_mode;
+}
